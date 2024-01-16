03cfeab9c86668515c24f7dc5462270e22613d66
strawhatluffy23 <strawhatluffy2333@gmail.com>
2024-01-16 20:37:19 +0000

    first commit on repo

---
+++ Resources/CSS/index.js
@@ -0,0 +1,28 @@
+// userInfo.js
+document.addEventListener('DOMContentLoaded', () => {
+    const browserPlaceholder = document.getElementById('browserPlaceholder');
+    const timezonePlaceholder = document.getElementById('timezonePlaceholder');
+
+    // Detect Browser
+    const userAgent = navigator.userAgent;
+    let browserName;
+    if (userAgent.match(/chrome|chromium|crios/i)) {
+        browserName = "Chrome";
+    } else if (userAgent.match(/firefox|fxios/i)) {
+        browserName = "Firefox";
+    } else if (userAgent.match(/safari/i)) {
+        browserName = "Safari";
+    } else if (userAgent.match(/opr\//i)) {
+        browserName = "Opera";
+    } else if (userAgent.match(/edg/i)) {
+        browserName = "Edge";
+    } else {
+        browserName = "Unknown";
+    }
+    browserPlaceholder.textContent = browserName;
+
+    // Detect Time Zone
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    timezonePlaceholder.textContent = timeZone;
+});
+
